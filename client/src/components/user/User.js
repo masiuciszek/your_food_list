ecec2cbc6687857b92b0e8699059951b6ac34ae7
Masiu <ciszekmarcell@gmail.com>
2019-11-24 18:05:40 +0100

    spelling change

--- client/src/components/user/User.js
+++ client/src/components/user/User.js
@@ -5,9 +5,9 @@ import { AuthContext } from '../../context/auth/auth.state';
 const User = props => {
   const { user, loading, isAuth } = React.useContext(AuthContext);
   return (
-    <div>
+    <>
       <h1>UserProfile {user && user.firstName}</h1>
-    </div>
+    </>
   );
 };
 
