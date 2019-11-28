22d1ee0310d516340b41df65bffe3c5b53534955
Marcell Ciszek <you@example.com>
2019-11-28 15:12:19 +0100

    ADD

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // @ts-nocheck
 const express = require('express');
-
+const cors = require('cors')
 const app = express();
 const connectDb = require('./db/connectDb');
 
 const port = process.env.PORT || 5000;
 app.use(express.json());
+app.use(cors())
 
 connectDb();
 
