2941368ca36f0467fd0340d621a22dec978b8d23
Masiu <ciszekmarcell@gmail.com>
2019-11-30 17:53:20 +0100

    add

--- ts-client-two/public/worker.js
+++ ts-client-two/public/worker.js
@@ -1,33 +1,38 @@
-const CACHE_NAME = 'pwa-task-manager';
-const urlsToCache = ['/', '/completed'];
+const CACHE_NAME = 'food-for-you';
+const urlsToCache = [
+  '/',
+  '/login',
+];
 
 // Install a service worker
 self.addEventListener('install', (event) => {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache');
-      return cache.addAll(urlsToCache);
-    }),
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      }),
   );
 });
 
 // Cache and return requests
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
-      return fetch(event.request);
-    }),
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }),
   );
 });
 
 // Update a service worker
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = ['pwa-task-manager'];
+  const cacheWhitelist = ['food-for-you'];
   event.waitUntil(
     caches.keys().then((cacheNames) => Promise.all(
       cacheNames.map((cacheName) => {
@@ -38,3 +43,34 @@ self.addEventListener('activate', (event) => {
     )),
   );
 });
+
+
+// NEW
+
+
+// self.addEventListener('install', (event) => {
+//   const offlineRequest = new Request('offline.html');
+//   event.waitUntil(
+//     fetch(offlineRequest).then((response) => caches.open('offline').then((cache) => {
+//       console.log('[oninstall] Cached offline page', response.url);
+//       return cache.put(offlineRequest, response);
+//     })),
+//   );
+// });
+
+// self.addEventListener('fetch', (event) => {
+//   const { request } = event;
+
+
+//   if (request.method === 'GET') {
+//     event.respondWith(
+//       fetch(request).catch((error) => {
+//         console.error(
+//           `[onfetch] Failed. Serving cached offline fallback ${
+//             error}`,
+//         );
+//         return caches.open('offline').then((cache) => cache.match('offline.html'));
+//       }),
+//     );
+//   }
+// });
